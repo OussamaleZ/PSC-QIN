0705182495e7e3ae796230ce6080d96bd8f25c27
OussamaleZ <zouhrydoussama@gmail.com>
2024-04-24 23:32:28 +0200

    Create routing.py

---
+++ routing.py
@@ -0,0 +1,26 @@
+#préparation des modules
+
+import pandas as pd
+import numpy as np
+from numpy import linalg
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+
+
+
+#prépa
+villes =[]
+
+
+#Création du graphe
+G=nx.Graph()
+
+G.add_nodes_from (["villeA",...])
+G.add_weighted_edges_from (edges_with_weights) #edges_with_weights doit etre une liste de triplets (villeA,villeB,probabilité)
+
+
+
+
+
+proba = f()
